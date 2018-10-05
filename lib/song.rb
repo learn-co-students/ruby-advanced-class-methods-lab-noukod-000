dadcef43a5433587439a7765dc3c4c31c68c62c3
Saint-Jean Germila <germila@noukod.com>
2018-10-05 18:44:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,7 @@
 class Song
-  attr_accessor :name, :artist_name
+
   @@all = []
 
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
 
 end
+Song.find_or_create_by_name("Blank Space")
